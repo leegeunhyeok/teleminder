c9676412cd14b12e5efc1690ee8f830e39fb8910
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-19 19:50:08 +0900

    UPDATE: Argument parse, config file read/write feature

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log('hello');
+import fs from 'fs';
+import arg from 'arg';
+
+interface BotRegistration {
+  token: string;
+  chatId: string;
+}
+
+const DefaultBotRegistration: BotRegistration = {
+  token: '',
+  chatId: '',
+};
+class Telenoty {
+  private static CONFIG_PATH = process.env.HOME + '/.telenoty';
+  private _debug = false;
+  private _bot: BotRegistration;
+
+  private createConfig() {
+    fs.writeFileSync(Telenoty.CONFIG_PATH, JSON.stringify(DefaultBotRegistration, null, 2));
+    this._bot = DefaultBotRegistration;
+  }
+
+  async init() {
+    return new Promise((resolve) => {
+      fs.readFile(Telenoty.CONFIG_PATH, 'utf-8', (err, data) => {
+        try {
+          err ? this.createConfig() : (this._bot = JSON.parse(data)) && resolve();
+        } catch (e) {
+          this._debug && console.log(e);
+        }
+      });
+    });
+  }
+}
+
+const args = arg({
+  '--regist': Boolean,
+  '--message': String,
+  // Simplify flags
+  '-r': Boolean,
+  '-m': String,
+});
+
+(async () => {
+  const instance = new Telenoty();
+  await instance.init();
+
+  if (args['--regist']) {
+    // TODO: Regist telegram bot
+  } else if (args['--message']) {
+    // TODO: Request message to bot
+  }
+})();
