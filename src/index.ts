6afcf2b81b776ee94f7b5a0cf2ec44a08a5f4dcd
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-19 20:16:44 +0900

    UPDATE: Checking CLI env, setBot/sendMessage methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import arg from 'arg';
+import axios from 'axios';
+
+const isCli = require.main === module;
 
 interface BotRegistration {
   token: string;
@@ -12,42 +15,77 @@ const DefaultBotRegistration: BotRegistration = {
 };
 class Telenoty {
   private static CONFIG_PATH = process.env.HOME + '/.telenoty';
+  private _bot: BotRegistration = null;
   private _debug = false;
-  private _bot: BotRegistration;
+
+  constructor(debug: boolean = false) {
+    this._debug = debug;
+  }
 
   private createConfig() {
     fs.writeFileSync(Telenoty.CONFIG_PATH, JSON.stringify(DefaultBotRegistration, null, 2));
     this._bot = DefaultBotRegistration;
   }
 
-  async init() {
+  async loadConfig() {
     return new Promise((resolve) => {
       fs.readFile(Telenoty.CONFIG_PATH, 'utf-8', (err, data) => {
         try {
           err ? this.createConfig() : (this._bot = JSON.parse(data)) && resolve();
         } catch (e) {
-          this._debug && console.log(e);
+          console.log(this._debug);
+          this._debug && console.error(e);
         }
       });
     });
   }
+
+  setBot(token: string, chatId: string) {
+    this._bot = { token, chatId };
+    return this;
+  }
+
+  async sendMessage(message: string) {
+    const { token, chatId } = this._bot;
+    if (token && chatId) {
+      axios.get(
+        `https://api.telegram.org/bot${token}/sendmessage?chat_id=${chatId}&text=${message}`,
+      );
+    } else {
+      throw new Error('token or chat id not provided');
+    }
+  }
 }
 
-const args = arg({
-  '--regist': Boolean,
-  '--message': String,
-  // Simplify flags
-  '-r': Boolean,
-  '-m': String,
-});
+export { Telenoty };
 
-(async () => {
-  const instance = new Telenoty();
-  await instance.init();
+// is CLI?
+if (isCli) {
+  try {
+    const args = arg({
+      '--regist': Boolean,
+      '--message': String,
+      '--debug': Boolean,
+      // Simplify flags
+      '-r': Boolean,
+      '-m': String,
+    });
 
-  if (args['--regist']) {
-    // TODO: Regist telegram bot
-  } else if (args['--message']) {
-    // TODO: Request message to bot
+    (async () => {
+      const instance = new Telenoty(args['--debug']);
+      await instance.loadConfig();
+
+      if (args['--regist']) {
+        // TODO: Regist telegram bot
+      } else if (args['--message']) {
+        // TODO: Request message to bot
+      }
+    })();
+  } catch (e) {
+    if (e.code === 'ARG_UNKNOWN_OPTION') {
+      console.error('telenoty: unknown option');
+    } else {
+      console.error('');
+    }
   }
-})();
+}
