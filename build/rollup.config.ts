6b39a88bc8a3e4f8302865c1dcab430689aafb1e
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-19 17:45:18 +0900

    INIT

---
+++ build/rollup.config.ts
@@ -0,0 +1,26 @@
+import { join } from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import license from 'rollup-plugin-license';
+
+export default {
+  input: join(__dirname, '../src/index.ts'),
+  output: [
+    {
+      format: 'cjs',
+      file: join(__dirname, '../dist/telenoty.js'),
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    license({
+      banner: {
+        commentStyle: 'none',
+        content: '#!/usr/bin/env node',
+      },
+    }),
+  ],
+};
